a00581a4ab0f44fa0a04e9a35b54f6166c1ddc58
Stéphane D. <stephanediraimondo@gmail.com>
2026-08-30 18:51:14 +0200

    Add files via upload

--- virtual.c
+++ virtual.c
@@ -64,7 +64,10 @@ void setup_virtual_device()
         snprintf(usetup.name, UINPUT_MAX_NAME_SIZE, "Microsoft X-Box 360 pad");
 
         usetup.id.bustype = BUS_USB;
-        usetup.id.vendor = 0x45e;
+        usetup.id.vendor = 0x45e;case KEY_SPACE:
+        printf("P1 bouton 3 -> North (Y)\n");
+        emit_north(val);
+        break;
         usetup.id.product = 0x28e;
         usetup.id.version = 0x110;
         usetup.ff_effects_max = 16;
@@ -166,50 +169,62 @@ void write_event(int event, int val)
                 w_pressed = (val > 0);
                 update_stick();
                 return;
+
         case KEY_A:
                 a_pressed = (val > 0);
                 update_stick();
                 return;
+
         case KEY_S:
                 s_pressed = (val > 0);
                 update_stick();
                 return;
+
         case KEY_D:
                 d_pressed = (val > 0);
                 update_stick();
                 return;
+
         case KEY_UP:
                 printf("P1 up -> hat0y = -1\n");
                 emit_hat0y(val ? -1 : 0);
                 break;
+
         case KEY_DOWN:
                 printf("P1 down -> hat0y = 1\n");
                 emit_hat0y(val ? 1 : 0);
                 break;
+
         case KEY_LEFT:
                 printf("P1 left -> hat0x = -1\n");
                 emit_hat0x(val ? -1 : 0);
                 break;
+
         case KEY_RIGHT:
                 printf("P1 right -> hat0x = 1\n");
                 emit_hat0x(val ? 1 : 0);
                 break;
+
         case KEY_LEFTCTRL:
                 printf("P1 button 1 -> South (A)\n");
                 emit_south(val);
                 break;
+
         case KEY_LEFTALT:
                 printf("P1 button 2 -> East (B)\n");
                 emit_east(val);
                 break;
+
         case KEY_SPACE:
-                printf("P1 button 3 -> West (X)\n");
-                emit_west(val);
+                printf("P1 bouton 3 -> North (Y)\n");
+                emit_north(val);
                 break;
+
         case KEY_1:
                 printf("P1 Start -> Start\n");
                 emit_start(val);
                 break;
+
         default:
                 return;
         }
